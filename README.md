2a55c0bd0a55a0cffbb2f90cf0cc5e09afd6e311
doristai <doris_tai@berkeley.edu>
2021-05-30 17:26:09 -0700

    update scraping.py and index.html

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # UCB-HTML-CSS-A10
+
+## Purpose 
+
+Build an app to scrape data from websites for this project and creating an HTML page to present our results 
+
+## Methods
+
+1. Using Mission_to_Mars_Challenge
+
+    - Writing code to retrieve the full-resolution image and title for each hemisphere image and added to the dictionary
+
+    - Create a list containing the dictionary of the full-resolution image URL string and title for each hemisphere image.
+
+2. Update the Web App with Mars’s Hemisphere Images and Titles 
+
+    - Creating new function in the ```scraping.py``` for updating hemipshere title and image 
+
+    - Updating ```index.html``` so website contains all the information collected 
--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres" : hemisphere(browser),
     }
 
     # Stop webdriver and return data
@@ -96,6 +97,58 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemisphere(browser):
+    # Create an empty dict to hold the hemisphere links
+    hemispheres = {}
+    
+    # Lead url to append to, extract
+    url_hemi = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url_hemi)
+    time.sleep(5)
+    hemi_soup = soup(browser.html, "html.parser")
+    headers = []
+    titles = hemi_soup.find_all("h3")
+    time.sleep(5)
+
+    for title in titles:
+        headers.append(title.text)
+    
+    images = []
+    count = 0
+    for thumb in headers:
+        browser.find_by_css("img.thumb")[count].click()
+        images.append(browser.find_by_text("Sample")["href"])
+        browser.back()
+        count = count+1
+    
+    hemisphere_image_urls = [] 
+    counter = 0
+    for item in images:
+        hemisphere_image_urls.append({"title": headers[counter],"img_url":images[counter]})
+        counter = counter+1
+    browser.back()
+    time.sleep(1)
+
+    return hemisphere_image_urls
+
+
+def scrape_hemisphere(html_text):
+    # parse html text
+    hemi_soup = soup(html_text, "html.parser")
+    # adding try/except for error handling
+    try:
+        title_elem = hemi_soup.find("h2", class_="title").get_text()
+        sample_elem = hemi_soup.find("a", text="Sample").get("href")
+    except AttributeError:
+        # Image error will return None, for better front-end handling
+        title_elem = None
+        sample_elem = None
+    hemispheres = {
+        "title": title_elem,
+        "img_url": sample_elem
+    }
+    return hemispheres
 
 if __name__ == "__main__":
 
